8e2b6c086f07585c4a51f95746c4ae635087233b
edoardoedr <edoardo.edr@gmail.com>
2023-10-05 22:12:22 +0200

    bug

--- AIxCT/pytorch_grad_cam/layer_cam.py
+++ AIxCT/pytorch_grad_cam/layer_cam.py
@@ -1,6 +1,6 @@
 import numpy as np
-from pytorch_grad_cam.base_cam import BaseCAM
-from pytorch_grad_cam.utils.svd_on_activations import get_2d_projection
+from .base_cam import BaseCAM
+from .utils.svd_on_activations import get_2d_projection
 
 # https://ieeexplore.ieee.org/document/9462463
 
